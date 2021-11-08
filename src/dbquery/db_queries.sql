28685f98f10629d3af2739b5023c740092d5d836
barabasizsolt <barabasi.robert.zsolt@gmail.com>
2021-11-08 23:28:10 +0200

    component_table_added

--- src/dbquery/db_queries.sql
+++ src/dbquery/db_queries.sql
@@ -6,6 +6,7 @@ DROP TABLE employee_position;
 DROP TABLE qa_engineer;
 DROP TABLE quality_check;
 DROP TABLE raw_material;
+DROP TABLE semi_component;
 
 # Measuer Unit table
 CREATE TABLE measure_unit(
@@ -16,6 +17,7 @@ CREATE TABLE measure_unit(
 
 INSERT INTO measure_unit (measure_unit_name, measure_unit_symbol) VALUES ("kilogramm", "kg");
 INSERT INTO measure_unit (measure_unit_name, measure_unit_symbol) VALUES ("litre", "l");
+INSERT INTO measure_unit (measure_unit_name, measure_unit_symbol) VALUES ("piece", "pcs");
 
 SELECT * FROM measure_unit;
 
@@ -85,7 +87,7 @@ CREATE TABLE quality_check(
 	quality_check_start_date DATETIME NOT NULL,
 	quality_check_end_date DATETIME NOT NULL,
 	quality_check_is_acceppted BOOL NOT NULL,
-	quality_check_notes VARCHAR(100) NULL,
+	quality_check_notes VARCHAR(100) NOT NULL,
     qa_engineer_id INT NOT NULL,
 	CONSTRAINT fk_qa_engineer_id
     FOREIGN KEY (qa_engineer_id) 
@@ -102,10 +104,10 @@ SELECT * FROM quality_check;
 #Raw Material Table
 CREATE TABLE raw_material(
 	raw_material_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	raw_material_name VARCHAR(50),
-	raw_material_origin VARCHAR(50),
-	raw_material_arrival_date DATETIME,
-	raw_material_lot_number VARCHAR(50),
+	raw_material_name VARCHAR(50) NOT NULL,
+	raw_material_origin VARCHAR(50) NOT NULL,
+	raw_material_arrival_date DATETIME NOT NULL,
+	raw_material_lot_number VARCHAR(50) NOT NULL,
     measure_unit_id INT NOT NULL,
 	CONSTRAINT fk_measure_unit_id
     FOREIGN KEY (measure_unit_id) 
@@ -128,15 +130,44 @@ INSERT INTO raw_material(raw_material_name, raw_material_origin, raw_material_ar
 measure_unit_id, delivery_company_id, courier_id, quality_check_id)
 VALUES ("rubber", "Germany", str_to_date('2021-02-14 13:12:01', '%Y-%m-%d %H:%i:%s'), "nr12345", 1, 1, 1, 1);
 
-select rm.raw_material_name, rm.raw_material_origin, rm.raw_material_arrival_date, rm.raw_material_lot_number,
+SELECT rm.raw_material_name, rm.raw_material_origin, rm.raw_material_arrival_date, rm.raw_material_lot_number,
 mu.measure_unit_name, dc.delivery_company_name, c.courier_name, qa.qa_engineer_name
-from raw_material rm join measure_unit mu on rm.measure_unit_id = mu.measure_unit_id
-join delivery_company dc on rm.delivery_company_id = dc.delivery_company_id
-join courier c on rm.courier_id = c.courier_id
-join quality_check qc on rm.quality_check_id = qc.quality_check_id
-join qa_engineer qa on qa.qa_engineer_id = qc.qa_engineer_id;
+FROM raw_material rm JOIN measure_unit mu ON rm.measure_unit_id = mu.measure_unit_id
+JOIN delivery_company dc ON rm.delivery_company_id = dc.delivery_company_id
+JOIN courier c ON rm.courier_id = c.courier_id
+JOIN quality_check qc ON rm.quality_check_id = qc.quality_check_id
+JOIN qa_engineer qa ON qa.qa_engineer_id = qc.qa_engineer_id;
 
+#Semi Component Table
+CREATE TABLE semi_component(
+	component_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	component_name VARCHAR(70) NOT NULL,
+    component_arrival_date DATETIME,
+	component_lot_number VARCHAR(50) NOT NULL,
+    measure_unit_id INT NOT NULL,
+	CONSTRAINT fkk_measure_unit_id
+    FOREIGN KEY (measure_unit_id) 
+        REFERENCES measure_unit(measure_unit_id),
+	delivery_company_id INT,
+	CONSTRAINT fkk_delivery_company_id
+    FOREIGN KEY (delivery_company_id) 
+        REFERENCES delivery_company(delivery_company_id),
+	courier_id INT,
+	CONSTRAINT fkk_courier_id
+    FOREIGN KEY (courier_id) 
+        REFERENCES courier(courier_id),
+	quality_check_id INT,
+	CONSTRAINT fkk_quality_check_id
+    FOREIGN KEY (quality_check_id) 
+        REFERENCES quality_check(quality_check_id)
+);
 
+INSERT INTO semi_component(component_name, component_arrival_date, component_lot_number, measure_unit_id, delivery_company_id, courier_id,
+quality_check_id)
+VALUES ("Transistor", str_to_date('2021-07-21 17:40:09', '%Y-%m-%d %H:%i:%s'), "nr3321ff", (select measure_unit_id from measure_unit where measure_unit_name = 'piece'),
+(select delivery_company_id from delivery_company where delivery_company_name = 'FAN Courier'),
+(select courier_id from courier where courier_name = 'John Doe'), 1);
 
+SELECT * FROM semi_component;
 
 
