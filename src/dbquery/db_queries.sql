436d43b344ce04a3fe18e92d9576b35b38902b94
barabasizsolt <barabasi.robert.zsolt@gmail.com>
2021-11-08 15:57:01 +0200

    delivery_company table created

--- src/dbquery/db_queries.sql
+++ src/dbquery/db_queries.sql
@@ -1,7 +1,8 @@
+# Measuer Unit table
 CREATE TABLE measure_unit(
 	measure_unit_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    measure_unit_name varchar(50) NOT NULL,
-    measure_unit_symbol varchar(10) NOT NULL
+    measure_unit_name VARCHAR(50) NOT NULL,
+    measure_unit_symbol VARCHAR(10) NOT NULL
 );
 
 DROP TABLE measure_unit;
@@ -10,3 +11,22 @@ INSERT INTO measure_unit (measure_unit_name, measure_unit_symbol) VALUES ("kilog
 INSERT INTO measure_unit (measure_unit_name, measure_unit_symbol) VALUES ("litre", "l");
 
 SELECT * FROM measure_unit;
+
+#Delivery Company Table
+CREATE TABLE delivery_company(
+	delivery_company_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	delivery_company_name VARCHAR(50) NOT NULL,
+	delivery_company_address VARCHAR(100) NOT NULL,
+	delivery_company_phone VARCHAR(20) NOT NULL,
+	delivery_company_email VARCHAR(50) NOT NULL
+);
+
+DROP TABLE delivery_company;
+
+INSERT INTO delivery_company (delivery_company_name, delivery_company_address, delivery_company_phone, delivery_company_email)
+VALUES ("FAN Courier", "Str Praidului 108, Sovata, Mures, 545500", "0732119212", "fan.courier@gmail.com");
+INSERT INTO delivery_company (delivery_company_name, delivery_company_address, delivery_company_phone, delivery_company_email)
+VALUES ("DPDgroup", "Str Lunga 36, Albesti, Mures, 5477025", "0722699733", "dpd.group@gmail.com");
+
+SELECT * FROM delivery_company;
+
